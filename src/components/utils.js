6e1e3a2cf7e797fa680f92770f0132eed600bf78
Michael Krukov <krukov.michael@ya.ru>
2019-12-05 15:11:47 +0300

    Update utils.js

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,9 +1,9 @@
 import { useEffect, useRef } from 'react';
 
 export const usePrevious = (value) => {
-    const ref = useRef(null);
-    useEffect(() => {
-      ref.current = value;
-    });
-    return ref.current;
-  };
+  const ref = useRef(null);
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+};
